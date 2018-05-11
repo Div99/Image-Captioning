516fe6754552ac47ab53644249695cd0891a8bb9
Divyansh Garg <dg595@cornell.edu>
2018-05-11 15:35:55 -0400

    Added generate_model script

--- CapGenerator/generate_model.py
+++ CapGenerator/generate_model.py
@@ -1,4 +1,37 @@
-import load_data
+import numpy as np
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
+
+# convert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+  all_desc = list()
+  for key in descriptions.keys():
+    [all_desc.append(d) for d in descriptions[key]]
+  return all_desc
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+  lines = to_lines(descriptions)
+  tokenizer = Tokenizer()
+  tokenizer.fit_on_texts(lines)
+  return tokenizer
+
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+  lines = to_lines(descriptions)
+  return max(len(d.split()) for d in lines)
 
 # create sequences of images, input sequences and output words for an image
 def create_sequences(tokenizer, max_length, descriptions, photos):
@@ -21,9 +54,27 @@ def create_sequences(tokenizer, max_length, descriptions, photos):
         X1.append(photos[key][0])
         X2.append(in_seq)
         y.append(out_seq)
-  return array(X1), array(X2), array(y)
+  return np.array(X1), np.array(X2), np.array(y)
 
-# calculate the length of the description with the most words
-def max_length(descriptions):
-  lines = to_lines(descriptions)
-  return max(len(d.split()) for d in lines)
+# define the captioning model
+def define_model(vocab_size, max_length):
+  # feature extractor model
+  inputs1 = Input(shape=(4096,))
+  fe1 = Dropout(0.5)(inputs1)
+  fe2 = Dense(256, activation='relu')(fe1)
+  # sequence model
+  inputs2 = Input(shape=(max_length,))
+  se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+  se2 = Dropout(0.5)(se1)
+  se3 = LSTM(256)(se2)
+  # decoder model
+  decoder1 = add([fe2, se3])
+  decoder2 = Dense(256, activation='relu')(decoder1)
+  outputs = Dense(vocab_size, activation='softmax')(decoder2)
+  # tie it together [image, seq] [word]
+  model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+  model.compile(loss='categorical_crossentropy', optimizer='adam')
+  # summarize model
+  print(model.summary())
+  plot_model(model, to_file='model.png', show_shapes=True)
+  return model

43e25c94c49a6eef825fd7f8c1e3d2709379042e
Divyansh Garg <dg595@cornell.edu>
2018-05-11 15:52:16 -0400

    more fixes

--- CapGenerator/train_model.py
+++ CapGenerator/train_model.py
@@ -1,4 +1,6 @@
 import load_data as ld
+import generate_model as gen
+from keras.callbacks import ModelCheckpoint
 
 # load dataset
 data = ld.prepare_dataset('train')
@@ -6,21 +8,21 @@ train_features, train_descriptions = data[0]
 test_features, test_descriptions = data[1]
 
 # prepare tokenizer
-tokenizer = create_tokenizer(train_descriptions)
+tokenizer = gen.create_tokenizer(train_descriptions)
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
 
 # determine the maximum sequence length
-max_length = max_length(train_descriptions)
+max_length = gen.max_length(train_descriptions)
 print('Description Length: %d' % max_length)
 
 # prepare sequences
-X1train, X2train, ytrain = create_sequences(tokenizer, max_length, train_descriptions, train_features)
-X1test, X2test, ytest = create_sequences(tokenizer, max_length, test_descriptions, test_features)
+X1train, X2train, ytrain = gen.create_sequences(tokenizer, max_length, train_descriptions, train_features)
+X1test, X2test, ytest = gen.create_sequences(tokenizer, max_length, test_descriptions, test_features)
 
 # fit model
 
-model = define_model(vocab_size, max_length)
+model = gen.define_model(vocab_size, max_length)
 # define checkpoint callback
 filepath = 'models/model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')

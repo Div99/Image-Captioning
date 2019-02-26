0d168e98c68d8d9ac2ba932ec48738930ef0ebad
Divyansh Garg <gaohuang.thu@gmail.com>
2019-02-25 22:52:44 -0500

    Added multi-model

--- CapGenerator/model.py
+++ CapGenerator/model.py
@@ -15,10 +15,10 @@ class Multi_Model:
     # will get dropped. So we pad with fake examples which are ignored
     # later on.
     if self.type == 'tpu':
-      total_samples = int(batch_size * (num_samples // batch_size + 1))
+      total_samples = batch_size * (num_samples // batch_size + 1)
       generator = tpu_gen(generator, num_samples % batch_size)
-      print('Steps: {}'.format(total_samples / batch_size))
-      features = self.keras_model.predict_generator(generator, total_samples / batch_size, verbose=verbose)
+      print('Steps: {}'.format(total_samples // batch_size))
+      features = self.keras_model.predict_generator(generator, total_samples // batch_size, verbose=verbose)
       return features[:num_samples]
 
     else:

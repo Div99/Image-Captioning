af352aa2800ba247b7f8ee1c213f2e4feccc92ab
Divyansh Garg <gaohuang.thu@gmail.com>
2019-02-25 22:53:22 -0500

    Added multi-model

--- CapGenerator/model.py
+++ CapGenerator/model.py
@@ -18,7 +18,7 @@ class Multi_Model:
       total_samples = batch_size * (num_samples // batch_size + 1)
       generator = tpu_gen(generator, num_samples % batch_size)
       print('Steps: {}'.format(total_samples // batch_size))
-      features = self.keras_model.predict_generator(generator, total_samples // batch_size, verbose=verbose)
+      features = self.keras_model.predict(generator, total_samples // batch_size, verbose=verbose)
       return features[:num_samples]
 
     else:
